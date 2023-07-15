95e20e67f57b8c9f8d7e000cedc81c39ba8faf9b
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-07-15 16:27:25 +0300

    task-01 +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+const BGCOLOR_CHANGE_KEY = 1000;
+let timerId = null;
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, BGCOLOR_CHANGE_KEY);
+
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
